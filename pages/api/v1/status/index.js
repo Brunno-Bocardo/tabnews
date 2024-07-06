a2ea04ab94c49a2fb74d5d42cbf343c0ea59e2b5
Brunno-Bocardo <brunno.pb@outlook.com>
2024-07-06 17:08:12 -0300

    finish `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
 import database from "infra/database.js"
 
 async function status (request, response) {
-    const result = await database.query('SELECT 1+1 as sum;')
-    console.log(result.rows)
-    response.status(200).json({chave: "valor"})
+    const updatedAt = new Date().toISOString()
+
+    const postgresVersionResult = await database.query('SHOW server_version;')
+    const postgresVersionValue = postgresVersionResult.rows[0].server_version
+
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections")
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+
+    // Esse método de sanitização evitam SQL injection 
+    // sendo uma ferramenta do próprio node-postgress 
+    // em quem a query é um objeto contendo o text (query) e values (valores injetados)
+    const databaseName = process.env.POSTGRES_DB
+    const databadeOpenedConnectionsResult = await database.query({
+        text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+        values: [databaseName]
+    })
+    const databadeOpenedConnectionsValue = databadeOpenedConnectionsResult.rows[0].count
+
+    
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: postgresVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue),
+                opened_connections: databadeOpenedConnectionsValue
+            }
+        }
+    })
+    
+    
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
+    // Aqui nós estamos fazendo um teste que escuta esse endpoint e 
+    // espera que o seu response.status seja 200 
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200);
+
+    // depois, pegamos o response (que veio como um texto puro) 
+    // e transformamos ele em um json, em algo que possamos mexer
+    const responseBody = await response.json()
+
+    // Aqui esperamos que, mesmo definida, o valor de updated_at 
+    // seja algo válido, e não algo aleatório (ou outra data)
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()    
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+
+    expect(responseBody.dependencies.database.version).toEqual("16.0")
+    expect(responseBody.dependencies.database.max_connections).toEqual(100)
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+
+    console.log(responseBody)
 })
